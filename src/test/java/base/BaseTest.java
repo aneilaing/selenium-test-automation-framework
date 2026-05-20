1cdf1e1568424ff0687c6a99178a8a611e75ab86
aneilaing <47797983+aneilaing@users.noreply.github.com>
2026-05-20 09:01:59 -0400

    Add DriverFactory for headless CI browser setup

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,9 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.time.Duration;
+import utils.DriverFactory;
 
 public class BaseTest {
 
@@ -14,14 +11,12 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = DriverFactory.createDriver();
     }
 
     @AfterMethod
     public void tearDown() {
+
         if (driver != null) {
             driver.quit();
         }
---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,23 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        return new ChromeDriver(options);
+    }
+}
